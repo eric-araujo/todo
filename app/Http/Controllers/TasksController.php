cbea4a1f242d749276a70e30312598402c6dd615
eric-araujo <eric.araujolima@hotmail.com>
2020-06-27 15:41:39 -0300

    Conexão com o banco de dados

--- app/Http/Controllers/TasksController.php
+++ app/Http/Controllers/TasksController.php
@@ -2,31 +2,14 @@
 
 namespace App\Http\Controllers;
 
+use App\Task;
 use Illuminate\Http\Request;
 
 class TasksController extends Controller
 {
     public function index() {
 
-        $tasks = [
-            [
-                "id" => 1,
-                "name" => "Estudar PHP 7",
-                "complete" => false,
-            ],
-            [
-                "id" => 2,
-                "name" => "Estudar ES 6",
-                "complete" => true,
-            ],
-            [
-                "id" => 3,
-                "name" => "Estudar NodeJs",
-                "complete" => false,
-            ],
-        ];
-    
-        return $tasks;
+        return Task::all();
 
     }
 
